995b70b9e319ff2994e391da348b9e354e434018
mehmetaltuntas20444 <mehmetaltuntas@alumni.sabanciuniv.edu>
2021-09-21 12:05:11 +0300

    Minor Changes and Comments

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 import getData from './lib/service.js';
 
-console.log(await getData(1));
+//homwework wants first call the function and get the data then log it so this is the case
+const data = await getData(1);
+console.log(data);
+
+//but we can also do it in one step both of them can stay so have fun :D
+// console.log(await getData(1));
--- src/lib/service.js
+++ src/lib/service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//we are using this link two times so make a simplification
 const apiURL = 'https://jsonplaceholder.typicode.com'
 
 const getData = async (userID) => {
