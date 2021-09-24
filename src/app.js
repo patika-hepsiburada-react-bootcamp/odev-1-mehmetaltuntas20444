77e723ab3d7c75c3ab62117f13a2c495408d753f
mehmetaltuntas20444 <mehmetaltuntas@alumni.sabanciuniv.edu>
2021-09-24 16:06:46 +0300

    Added one more usage of await

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import getData from './lib/service.js';
-
 //homwework wants first call the function and get the data then log it so this is the case
+(async () => {
 const data = await getData(1);
 console.log(data);
+})()
+
+//using await top level
+// const data = await getData(1);
+// console.log(data);
 
 //but we can also do it in one step both of them can stay so have fun :D
 // console.log(await getData(1));
